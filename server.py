7cd9e7ad62337eca8ac78d62013df4124a522129
vagrant <vagrant@vagrant.vm>
2019-11-08 06:49:27 +0000

    tested api on server. windows machine glitch, must end virtualbox tasks and do vagrant up again

--- server.py
+++ server.py
@@ -3,11 +3,26 @@ import requests
 from jinja2 import StrictUndefined
 from model import connect_db, db, Song, User, Annotation, seed_data
 
+# to access api key
+import os
+
 
 app = Flask(__name__)
 app.secret_key = 'sekrit'
 
 app.jinja_env.undefined = StrictUndefined
+
+GENIUS_TOKEN = os.environ.get('TOKEN')
+# GENIUS_URL = "http://104.17.212.67/"
+GENIUS_URL = "https://api.genius.com/"
+
+print('test')
+print('genius url:', GENIUS_URL)
+print('genius token:', GENIUS_TOKEN)
+
+@app.route("/")
+def homepage():
+    return render_template("index.html")
 
 @app.route("/test-query")
 def test_query():
@@ -18,11 +33,50 @@ def test_query():
     return render_template("test-query.html",
                             q=q)
 
-# @app.route("/search")
-# def search_api():
+@app.route("/test-input", methods=['POST'])
+def test_input():
+    """Testing get user input and save to database"""
+
+    annotation = request.form["annotation"]
+    fragment = request.form["fragment"]
+
+    # hard code values right now
+    user_id = 1
+    song_id = 2
+
+    new_annotation = Annotation(annotation=annotation, song_fragment=fragment, 
+                            user_id=user_id,
+                            song_id=song_id)
+
+    db.session.add(new_annotation)
+    db.session.commit()
+
+    # flash message did not work
+    flash(f"Annotation {annotation} added.")
+
+    return redirect("/test-query")
 
 
-#     return
+@app.route("/search")
+def search_api():
+
+
+    # artist = 'lil nas x'
+    title = 'panini'
+
+    # return artist
+
+    payload = {'access_token' : GENIUS_TOKEN,
+                'q': title}
+    url = GENIUS_URL + "search"
+    # print(url)
+    response = requests.get(url, params=payload)
+
+    # print(response.content)
+    data = response.json()
+
+    return data['response']['hits'][0]['result']['full_title']
+
 
 # @app.route("/anno-form")
 # def annoform():
