1077b0bbd36b14a3521c16a1f0fba648b54d1abb
Dominique Cuevas <cuevasdominique@gmail.com>
2019-11-28 13:28:34 -0800

    added song annotations from db

--- static/app.jsx
+++ static/app.jsx
@@ -8,7 +8,9 @@ class App extends React.Component {
             video: "",
             songDataLoaded: false,
             fragment: "",
-            catData: { display: 'none' }
+            catData: { display: 'none' },
+            songAnnotations: false,
+            annotations: ""
         };
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,6 +23,20 @@ class App extends React.Component {
         this.setState({catData: null });
 
         $.get(`/api/search?${q}`, (res) => {
+            if (res.song_annos.length) {
+                this.setState({ songAnnotations: true });
+                let annotations = "<tr><th>Fragment</th><th>Annotation</th></tr>"
+                for (const song_anno of res.song_annos) {
+                    annotations+=`<tr id="${song_anno['anno_id']}"></tr>`
+                    annotations+=`<td>${song_anno['song_fragment']}</td>`
+                    annotations+=`<td>${song_anno['annotation']}</td>`
+                    annotations+="</tr>"
+                };
+                this.setState({ annotations: annotations });
+            } else {
+                this.setState({ songAnnotations: false });
+            };
+
             this.setState({
                 title: res.song_title, 
                 artist: res.song_artist,
@@ -44,7 +60,13 @@ class App extends React.Component {
         let displayData = { display: 'none' };
         if (this.state.songDataLoaded) {
             displayData = null;
-        }
+        };
+        let displayAnnos = { display: 'none' };
+        if (this.state.songAnnotations) {
+            displayAnnos = null;
+        } else {
+            displayAnnos = { display: 'none' };
+        };
 
         return (
             <div>
@@ -53,13 +75,16 @@ class App extends React.Component {
                     <input type="submit" value="Search" />
                 </form>
 
-                <img style={this.state.catData} width="100" src="/static/img/loadnyan.gif" />
-
-                <div className="song-data" style={displayData}>
+                <span style={this.state.catData}>
+                &nbsp;<img id="catGif" height="16px" src="/static/img/nyancat.gif" />
+                &nbsp;Loading...
+                </span>
+                <div id="song-data" style={displayData}>
+                    <br />
                     <iframe src={this.state.video} type="text/html" frameBorder="0" width="640" height="360"></iframe>
                     <h2>{this.state.title}</h2>
                     <h3>{this.state.artist}</h3>
-                    <p dangerouslySetInnerHTML={{__html: this.state.lyrics}}></p>
+                    <div id="lyrics" dangerouslySetInnerHTML={{__html: this.state.lyrics}}></div>
                     <p>
                     <button onClick={this.handleSelection} id="get-fragment">Copy song fragment</button>
                     </p>
@@ -77,6 +102,8 @@ class App extends React.Component {
                         <input type="hidden" name="video_url" value={this.state.video} />
                         <input type="submit" value="Save" />
                     </form>
+                    <table id="q_annotations" style={displayAnnos} dangerouslySetInnerHTML={{__html: this.state.annotations}}>
+                    </table>
                 </div>
             </div>
         );
