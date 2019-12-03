0841627dbee63c33b919076b66bbab5ed589ade7
Dominique Cuevas <cuevasdominique@gmail.com>
2019-12-02 16:27:00 -0800

    removed commented out code

--- server.py
+++ server.py
@@ -24,83 +24,7 @@ GENIUS_URL = "https://api.genius.com/"
 @app.route("/")
 def homepage():
 
-    # if not session.get('current_user'):
-    #     return redirect("/user-reg")
-
     return render_template("reacthits.html")
-
-# @app.route("/react")
-# def react():
-
-#     return render_template("react.html")
-
-# @app.route("/results")
-# def results():
-
-#     search = request.args.get('q') # 'q' from index.html form input
-
-#     payload = {'access_token' : GENIUS_TOKEN,
-#                 'q': search}
-#     url = GENIUS_URL + "search"
-#     # print(url)
-#     response = requests.get(url, params=payload)
-
-#     # print(response.content)
-#     data = response.json()
-
-#     # go to search api > songs api > youtube video
-#     song_title = data['response']['hits'][0]['result']['title']
-#     artist = data['response']['hits'][0]['result']['primary_artist']['name']
-#     lyrics_url = data['response']['hits'][0]['result']['url']
-#     session['song_title'] = song_title
-#     session['song_artist'] = artist
-
-#     api_song = data['response']['hits'][0]['result']['api_path']
-
-#     payload_song = {'access_token : GENIUS_TOKEN'}
-#     url_song = GENIUS_URL + api_song.lstrip('/') # get rid of second slash
-#     response_song = requests.get(url_song, params=payload)
-#     data_song = response_song.json()
-
-#     # video_url = data_song['response']['song']['media'][0]['url']
-#     # session['video_url'] = video_url
-
-#     media_list = data_song['response']['song']['media']
-#     for idx, media in enumerate(media_list):
-#         if media['provider']=="youtube":
-#             session['video_url'] = media_list[idx]['url']
-#             video_url = session['video_url'].replace("http://www.youtube.com/watch?v=","")
-
-#     # web scraping
-#     page = requests.get(lyrics_url)
-#     # make Beautiful Soup elements from DOM
-#     soup = BeautifulSoup(page.text, 'html.parser')
-#     # from the webpage, get back the html element with the 'lyrics' class
-#     lyrics = soup.find(class_='lyrics')
-#     # lyrics as string with \n
-#     lyrics_str = lyrics.get_text()
-#     # replaced python's \n to html <br>, still in quotes
-#     lyrics_html = lyrics_str.replace('\n','<br>')
-#     session['lyrics'] = lyrics_html
-
-#     # query any annotations for song searched
-#     q_annotations = Annotation.query.filter(Song.song_title==session['song_title'],
-#                                             Song.song_artist==session['song_artist']).join(Song).all()
-
-#     return render_template("results.html", 
-#                             song_title=song_title,
-#                             artist=artist,
-#                             lyrics_html=lyrics_html,
-#                             video_url=video_url,
-#                             q_annotations=q_annotations)
-
-# @app.route("/hits")
-# def hits():
-
-#     # if not session.get('current_user'):
-#     #     return redirect("/user-reg")
-
-#     return render_template("reacthits.html")
 
 @app.route("/api/hits")
 def api_hits():
@@ -110,7 +34,7 @@ def api_hits():
     search_dict = genius_hits.search(search)
 
     return jsonify(search_dict)
-
+    
 
 @app.route("/api/search")
 def api_search():
@@ -139,6 +63,7 @@ def api_search():
     search_dict['song_annos'] = song_annos
 
     return jsonify(search_dict)
+
 
 @app.route("/annosongs.json")
 def songs():
@@ -176,6 +101,7 @@ def user_session():
     print(session['current_user'])
 
     return redirect("/")
+
 
 @app.route("/save", methods=['POST'])
 def save():
@@ -217,6 +143,7 @@ def save():
 
     return redirect("/user-annos")
 
+
 @app.route("/user-annos")
 def user_annos():
 
@@ -230,18 +157,6 @@ def user_annos():
         flash('Please sign-in')
         return redirect('/user-reg')
 
-# @app.route("/json/user-annos")
-# def user_annos():
-
-#     if session.get('current_user'):
-#         user = User.query.get(session['current_user'])
-#         annotations = user.annotations
-#         return render_template("user_annotations.html",
-#                                 user=user,
-#                                 annotations=annotations)
-#     else:
-#         flash('Please sign-in')
-#         return redirect('/user-reg')
 
 @app.route("/annosongs")
 def annosongs():
@@ -249,13 +164,6 @@ def annosongs():
     songs = Song.query.all()
     return render_template("songs.html",
                             songs = songs)
-
-# @app.route("/songs/<int:song_id>")
-# def song(song_id):
-
-#     song = Song.query.get(song_id)
-#     return render_template("song.html",
-#                             song=song)
 
 if __name__ == "__main__":
     connect_db(app)
